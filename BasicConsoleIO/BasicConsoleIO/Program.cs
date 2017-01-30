3849916a07a5a84cfcf7587a06e105e731e316f0
Steff McKee <steffmckee@outlook.com>
2017-01-29 19:39:09 -0700

    initial commit

---
+++ BasicConsoleIO/BasicConsoleIO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsoleIO
+{
+    class Program
+    {
+        private static void GetUserData()
+        {
+            // Get name and age.
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+            // Change echo color, just for fun.
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Echo to the console.
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+            // Restore previous color.
+            Console.ForegroundColor = prevColor;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Console I/O *****");
+            GetUserData();
+            Console.ReadLine();
+        }
+    }
+}
